36db8faf8904b2efbc79ac292b4dc3ad139d0ca5
Logan St. John <loganwst.john@gmail.com>
2024-02-12 10:33:10 -0500

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,15 @@
-import { useState, useEffect } from 'react'
 import { useAuth } from '../context/AuthContext'
 import { toast } from 'react-hot-toast'
 import { useNavigate } from 'react-router-dom'
 
 const Header = () => {
-    // const [user, setUser] = useState()
     const navigate = useNavigate()
     const { user, logOut } = useAuth()
-
-    // useEffect(() => {
-    //     const userData = getUser()
-    //     setUser(userData)
-    // }, [])
 
     const logout = () => {
         logOut()
         toast.success('Logged out!')
-        navigate('/login')
+        navigate('/')
     }
 
     return (
